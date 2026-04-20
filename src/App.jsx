2d9b3d450ce5c9e94c39b8d38b8305a78170bc13
Alok Singh <ialksng@gmail.com>
2026-04-20 07:17:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import IndraWebsite from './pages/IndraWebsite';
 import IndraWidget from './pages/IndraWidget';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<IndraWebsite />} />
         <Route path="/widget" element={<IndraWidget />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
